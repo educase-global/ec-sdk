cd647e0b0e75765ca8a0bc8d3ab557c776bdd923
sourav pahwa <Sourav61pahwa@gmail.com>
2021-09-20 17:27:02 +0530

    API_URN_TESTS_SHOW_ADMIN API_ADDED
    API_URN_TESTS_SHOW_ADMIN

--- _test/test.js
+++ _test/test.js
@@ -11,6 +11,22 @@ const EC_SDK = require("ec-sdk")(
 )
 
 // Test Report api function
+// EC_SDK.TestReport.testsShowAdmin(
+//   function (err, data) {
+//     if (err) {
+//       return console.log('something went wrong');
+//     }
+//     console.log('data', data)
+//   },
+//   {
+//     auth: auth_bearer_token_admin,
+//     params: {
+//       id: "1"
+//     }
+//   }
+// );
+
+// Test Report api function
 // EC_SDK.TestReport.testsStoreAdmin(
 //   function (err, data) {
 //     if (err) {
