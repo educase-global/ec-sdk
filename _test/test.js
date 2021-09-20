d624b2bf5c420ad5851805c2bfafb128cb1d62f4
sourav pahwa <Sourav61pahwa@gmail.com>
2021-09-20 17:53:06 +0530

    API_URN_STUDENT_TEST_TABLE_DATA API_ADDED
    API_URN_STUDENT_TEST_TABLE_DATA

--- _test/test.js
+++ _test/test.js
@@ -11,6 +11,25 @@ const EC_SDK = require("ec-sdk")(
 )
 
 // Test Report api function
+// EC_SDK.TestReport.studentTestsTableDataAdmin(
+//   function (err, data) {
+//     if (err) {
+//       return console.log('something went wrong');
+//     }
+//     console.log('data', data)
+//   },
+//   {
+//     auth: auth_bearer_token_admin,
+//     params: {
+//       perPage: "10",
+//       page: "0",
+//       subject_id: "",
+//       exam_type: ""
+//     }
+//   }
+// );
+
+// Test Report api function
 // EC_SDK.TestReport.testsReportDataUpdateAdmin(
 //   function (err, data) {
 //     if (err) {
