6938e8855bdf2855c44f0eb929c598a1de08b345
sourav pahwa <Sourav61pahwa@gmail.com>
2021-09-23 22:18:56 +0530

    API_URN_SMS_TEMPLATE_DATA_ADMIN API_ADDED
    API_URN_SMS_TEMPLATE_DATA_ADMIN

--- _test/test.js
+++ _test/test.js
@@ -11,18 +11,34 @@ const EC_SDK = require("ec-sdk")(
 );
 
 // SmsEmail api function
-EC_SDK.SmsEmail.smsTemplateDropdownAdmin(
-	function (err, data) {
-		if (err) {
-			return console.log("something went wrong");
-		}
-		console.log("data", data);
-	},
-	{
-		auth: auth_bearer_token_admin,
-		params: {},
-	}
-);
+// EC_SDK.SmsEmail.smsTemplateDataAdmin(
+// 	function (err, data) {
+// 		if (err) {
+// 			return console.log("something went wrong");
+// 		}
+// 		console.log("data", data);
+// 	},
+// 	{
+// 		auth: auth_bearer_token_admin,
+// 		params: {
+// 			id: "1",
+// 		},
+// 	}
+// );
+
+// SmsEmail api function
+// EC_SDK.SmsEmail.smsTemplateDropdownAdmin(
+// 	function (err, data) {
+// 		if (err) {
+// 			return console.log("something went wrong");
+// 		}
+// 		console.log("data", data);
+// 	},
+// 	{
+// 		auth: auth_bearer_token_admin,
+// 		params: {},
+// 	}
+// );
 
 // SmsEmail api function
 // EC_SDK.SmsEmail.sendSmsEmailEnquiryAdmin(
