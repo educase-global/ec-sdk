09ac2682f8c7bf22c9215a3f150caafc6509fe1f
sourav pahwa <Sourav61pahwa@gmail.com>
2021-09-29 12:17:57 +0530

    API_URN_GET_TEACHER_SIGNATURE_DROPDOWN API_ADDED
    API_URN_GET_TEACHER_SIGNATURE_DROPDOWN

--- _test/test.js
+++ _test/test.js
@@ -10,6 +10,20 @@ const EC_SDK = require("ec-sdk")(
 	APP_CONFIG
 );
 
+// // dropdown api function
+// EC_SDK.Dropdown.getTeacherSignatureDropdown(
+// 	function (err, data) {
+// 		if (err) {
+// 			return console.log("something went wrong");
+// 		}
+// 		console.log("data", data);
+// 	},
+// 	{
+// 		auth: auth_bearer_token_admin,
+// 		params: {},
+// 	}
+// );
+
 // SmsEmail api function
 // EC_SDK.SmsEmail.smsTemplateDataAdmin(
 // 	function (err, data) {
